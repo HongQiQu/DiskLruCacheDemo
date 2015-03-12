26128260a65b51e41e9b06369854d953e0e0bf0a
Zhenguo <jingle1267@163.com>
2015-03-12 15:55:34 +0800

    [NewFeature] Write file and read file.

---
+++ src/com/ihongqiqu/cache/CommonUtils.java
@@ -0,0 +1,124 @@
+package com.ihongqiqu.cache;
+
+import android.content.Context;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.os.Environment;
+import android.text.TextUtils;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * 缓存相关方法
+ *
+ * Created by zhenguo on 3/12/15.
+ */
+public class CommonUtils {
+
+    /**
+     * 获取缓存地址
+     * @param context
+     * @param uniqueName 缓存文件夹名称
+     * @return
+     */
+    public static File getDiskCacheDir(Context context, String uniqueName) {
+        String cachePath;
+        if ((Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())
+                || !Environment.isExternalStorageRemovable())
+                && context.getExternalCacheDir() != null && !TextUtils.isEmpty(context.getExternalCacheDir().getPath())) {
+            cachePath = context.getExternalCacheDir().getPath();
+        } else {
+            cachePath = context.getCacheDir().getPath();
+        }
+        return new File(cachePath + File.separator + uniqueName);
+    }
+
+    /**
+     * 获取应用版本号
+     * @param context
+     * @return
+     */
+    public static int getAppVersion(Context context) {
+        try {
+            PackageInfo info = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
+            return info.versionCode;
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+        return 1;
+    }
+
+    /**
+     * 将url写入缓存
+     * @param urlString
+     * @param outputStream
+     * @return
+     */
+    public static boolean downloadUrlToStream(String urlString, OutputStream outputStream) {
+        HttpURLConnection urlConnection = null;
+        BufferedOutputStream out = null;
+        BufferedInputStream in = null;
+        try {
+            final URL url = new URL(urlString);
+            urlConnection = (HttpURLConnection) url.openConnection();
+            in = new BufferedInputStream(urlConnection.getInputStream(), 8 * 1024);
+            out = new BufferedOutputStream(outputStream, 8 * 1024);
+            int b;
+            while ((b = in.read()) != -1) {
+                out.write(b);
+            }
+            return true;
+        } catch (final IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            try {
+                if (out != null) {
+                    out.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (final IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 将字符串惊醒MD5编码
+     * @param key
+     * @return
+     */
+    public static String hashKeyForDisk(String key) {
+        String cacheKey;
+        try {
+            final MessageDigest mDigest = MessageDigest.getInstance("MD5");
+            mDigest.update(key.getBytes());
+            cacheKey = bytesToHexString(mDigest.digest());
+        } catch (NoSuchAlgorithmException e) {
+            cacheKey = String.valueOf(key.hashCode());
+        }
+        return cacheKey;
+    }
+
+    private static String bytesToHexString(byte[] bytes) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < bytes.length; i++) {
+            String hex = Integer.toHexString(0xFF & bytes[i]);
+            if (hex.length() == 1) {
+                sb.append('0');
+            }
+            sb.append(hex);
+        }
+        return sb.toString();
+    }
+
+}
--- src/com/ihongqiqu/cache/MainActivity.java
+++ src/com/ihongqiqu/cache/MainActivity.java
@@ -1,15 +1,99 @@
 package com.ihongqiqu.cache;
 
 import android.app.Activity;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import com.jakewharton.disklrucache.DiskLruCache;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+
+    private DiskLruCache mDiskLruCache = null;
+    private ImageView preview_iv;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        init();
     }
+
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.write_cache_btn:
+                writeCache();
+                break;
+            case R.id.read_chache_btn:
+                readCache();
+                break;
+        }
+    }
+
+    private void init() {
+        preview_iv = (ImageView) findViewById(R.id.preview_iv);
+
+        try {
+            File cacheDir = CommonUtils.getDiskCacheDir(this, "bitmap");
+            if (!cacheDir.exists()) {
+                cacheDir.mkdirs();
+            }
+            mDiskLruCache = DiskLruCache.open(cacheDir, CommonUtils.getAppVersion(this), 1, 10 * 1024 * 1024);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    /**
+     * 写缓存
+     */
+    private void writeCache() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    String imageUrl = "http://img.my.csdn.net/uploads/201309/01/1378037235_7476.jpg";
+                    String key = CommonUtils.hashKeyForDisk(imageUrl);
+                    DiskLruCache.Editor editor = mDiskLruCache.edit(key);
+                    if (editor != null) {
+                        OutputStream outputStream = editor.newOutputStream(0);
+                        if (CommonUtils.downloadUrlToStream(imageUrl, outputStream)) {
+                            editor.commit();
+                        } else {
+                            editor.abort();
+                        }
+                    }
+                    mDiskLruCache.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    /**
+     * 读缓存
+     */
+    private void readCache() {
+        try {
+            String imageUrl = "http://img.my.csdn.net/uploads/201309/01/1378037235_7476.jpg";
+            String key = CommonUtils.hashKeyForDisk(imageUrl);
+            DiskLruCache.Snapshot snapShot = mDiskLruCache.get(key);
+            if (snapShot != null) {
+                InputStream is = snapShot.getInputStream(0);
+                Bitmap bitmap = BitmapFactory.decodeStream(is);
+                preview_iv.setImageBitmap(bitmap);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
